f3d22c217ec2ffd8c011c4397e1cb1ed4dfd4458
Noman172590 <nomanjahan866615@gmail.com>
2023-09-28 10:31:06 +0600

    First release of Hello World!

--- src/Component/Product/Product.jsx
+++ src/Component/Product/Product.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import './Product.css'
 
 const Product = (props) => {
-    console.log(props.ShowProduct);
-    const { img, name, price, seller, ratings } = props.ShowProduct
+    const buttonOnclick=props.buttonOnclick;
+    const { img, name, price, seller, ratings } = props.ShowProduct;
+   
     return (
         <div className='product'>
             <div >
             <img src={img} alt="" />
-                <h5>{name}</h5>
+            <h5>{name}</h5>
                 <h4>Price:$ {price}</h4>
                 <h3>Manufacturer:{seller}</h3>
                 <h2>Rating: {ratings}<span style={{ color: "red" }}> start</span></h2>
                
             </div>
             <div>
-            <button className='btn_card'>Add to Card</button>
+            <button onClick={()=>buttonOnclick(props.ShowProduct)} className='btn_card'>Add to Card <FontAwesomeIcon icon={faShoppingCart} />    </button>
             </div>
            
         </div>
--- src/Component/Shop/Shop.jsx
+++ src/Component/Shop/Shop.jsx
@@ -4,20 +4,28 @@ import Product from '../product/product';
 
 const Shop = () => {
     const [product,setProduct]=useState([])
+    const [card,setCard]=useState([]);
     useEffect(()=>{
         fetch('products.json')
         .then(res=>res.json())
         .then(data=>setProduct(data))
-    })
+        
+    },[]);
+    const buttonOnclick=(Product)=>{
+        const newCard=[...card,product];
+        setCard(newCard)
+    }
     return (
         <div className='shopmaking'>
            <div className='SHowcontainer'>
            {
-                product.map(products=><Product key={products.id} ShowProduct={products}></Product>)
+                product.map(products=><Product key={products.id} ShowProduct={products} buttonOnclick={buttonOnclick}></Product>)
+                
               }
            </div>
            <div>
-           <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, distinctio?</h3>
+           <h1>Order Summary</h1>
+           <h2>Selected Items: {card.length}</h2>
             
             </div>  
                
